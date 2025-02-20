ec33a4e875b02bc500c7461c00ad71498ae4fe30
SamuelWilensky <114035374+SamuelWilensky@users.noreply.github.com>
2025-02-19 20:24:45 -0500

    game and profile post paths

--- gamedecide/backend/server.js
+++ gamedecide/backend/server.js
@@ -19,5 +19,138 @@ async function getCollections(){
     groups = await dbconnect.db(db).collection("groups");
     games = await dbconnect.db(db).collection("games");
 }
-
 getCollections();
+
+//#region mongodb queries
+async function SendAllGames(res){
+    const result = await games.find().toArray();
+    res.end(JSON.stringify(result));
+}
+
+async function insertIntoCollection(data, collection){
+    await collection.insertOne(data);
+}
+
+async function AttemptUpdateGame(data){
+    const result = await games.replaceOne({name:data.name, year:data.year}, data);
+
+    if(result.modifiedCount === 0){
+        await insertIntoCollection(data, games);
+    }
+}
+
+async function SendAllProfileNames(username, res){
+    const result = await profiles.find({username:username}).toArray();
+    const names = [];
+    for(let i=0; i<result.length; i++){
+        names.push(result[i].name);
+    }
+    res.end(JSON.stringify(names));
+}
+
+async function SendProfile(username, name, res){
+    const result = await profiles.findOne({username:username, name:name});
+    res.end(JSON.stringify(result));
+}
+
+async function AttemptUpdateProfile(data){
+    const result = await profiles.replaceOne({username:data.username, name:data.name}, data);
+
+    if(result.modifiedCount === 0){
+        await insertIntoCollection(data, profiles);
+    }
+}
+//#endregion
+
+app.post("/findgame", (req, res) => {
+    req.on("end", function () {
+        SendAllGames(res);
+
+    })
+
+})
+
+app.post("/submitgame", (req, res) => {
+    let dataString = ""
+
+    req.on("data", function (data) {
+
+        dataString += data
+
+    })
+
+    req.on("end", function () {
+        const data = JSON.parse(dataString);
+
+        if(data.name === null || data.name === ""){
+            res.end("Not submitted");
+            return;
+        }
+
+        if(data.year === null || data.year === ""){
+            data.year = "none";
+        }
+
+        AttemptUpdateGame(data).then(()=>{
+            res.end("Submitted");
+        })
+
+    })
+
+})
+
+app.post("/getprofiles", (req, res) => {
+    let dataString = ""
+
+    req.on("data", function (data) {
+
+        dataString += data
+
+    })
+
+    req.on("end", function () {
+        const data = JSON.parse(dataString);
+
+        SendAllProfileNames(data, res);
+    })
+})
+
+app.post("/editprofile", (req, res) => {
+    let dataString = ""
+
+    req.on("data", function (data) {
+
+        dataString += data
+
+    })
+
+    req.on("end", function () {
+        const data = JSON.parse(dataString);
+
+        SendProfile(data.username, data.name, res);
+    })
+})
+
+app.post("/submitprofile", (req, res) => {
+    let dataString = ""
+
+    req.on("data", function (data) {
+
+        dataString += data
+
+    })
+
+    req.on("end", function () {
+        const data = JSON.parse(dataString);
+
+        if(data.name === null || data.name === ""){
+            res.end("Not submitted");
+            return;
+        }
+
+        AttemptUpdateProfile(data).then(()=>{
+            res.end("Submitted");
+        })
+    })
+})
+
