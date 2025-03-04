8f87ffbf945d467696808fe670d6ea6adca64aa0
Justin Yip <jyip1@wpi.edu>
2025-03-04 01:12:34 -0500

    Added some scroll

--- gamedecide/frontend/src/GameSearch.jsx
+++ gamedecide/frontend/src/GameSearch.jsx
@@ -74,11 +74,17 @@ function GameSearch({user}) {
         <>
             <Container maxWidth="sm" className="flex flex-col justify-start items-center gap-4">
                 <H1Component text={"Search for Games"}/>
-                <Box className="w-full p-8 rounded-md" bgcolor="cardBG.main">
+                <Box className="flex flex-col justify-center items-center w-full p-4 rounded-md" bgcolor="cardBG.main">
                     <AutocompleteComponent id={"gamesSearch"} label={"Search for Games"} map={mapGames} list={games}
                                            input={search} onChange={handleSearch} autocompleteChange={handleAutocompleteChange} error={false} errorMessage={""}/>
                 </Box>
-                <Box sx={{ mt: 4 }} className="w-full flex flex-col gap-4 p-8 rounded-md" bgcolor="cardBG.main">
+                <Box className="w-full flex flex-col gap-4 p-8 rounded-md" bgcolor="cardBG.main"
+                     sx={{
+                         mt: 4,
+                         height: 500,
+                         overflow: "hidden",
+                         overflowY: "scroll",
+                     }}>
                     {filteredGames.map(game => (
                         <GameComponent key={game.name + "(" + game.year + ")"} game={game} functions={{handleDelete: handleDelete}}/>
                     ))}
